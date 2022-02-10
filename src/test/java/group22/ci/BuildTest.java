33c1d852d9c6f9bb912de778a27e4434df2e2006
Johan <jekbe@kth.se>
2022-02-10 19:37:12 +0100

    Fix issue/10
    Adds three tests to test three different projects. Uses the correct
    boolean result for the maven commands.

--- src/test/java/group22/ci/BuildTest.java
+++ src/test/java/group22/ci/BuildTest.java
@@ -1,41 +1,50 @@
 package group22.ci;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit tests for testing the validation of compilation and tests
  */
 public class BuildTest 
 {
     /**
-     * Rigorous Test :-)
+     * Tests a project without problems
+     * Should compile and pass its tests
      */
     @Test
     public void successful_build()
     {
         String path = "mvn_test_projects/successful/demo";
-        ContinuousIntegrationServer.mavenCompile(path);
-        ContinuousIntegrationServer.mavenTest(path);
-        assertTrue( true );
+        boolean comp_result = ContinuousIntegrationServer.mavenCompile(path).result;
+        assertTrue(comp_result);
+        boolean test_result = ContinuousIntegrationServer.mavenTest(path);
+        assertTrue(test_result);
     }
 
+    /**
+     * Tests a project with compilation error
+     * Should fail to compile
+     */
     @Test
     public void fail_compilation()
     {
         String path = "mvn_test_projects/failcompile/demo";
-        ContinuousIntegrationServer.mavenCompile(path);
-        ContinuousIntegrationServer.mavenTest(path);
-        assertTrue( true );
+        boolean comp_result = ContinuousIntegrationServer.mavenCompile(path).result;
+        assertFalse(comp_result);
     }
 
+    /**
+     * Tests a project with a failing test
+     * Should not pass all its tests
+     */
     @Test
     public void fail_tests()
     {
         String path = "mvn_test_projects/failTest/demo";
-        ContinuousIntegrationServer.mavenCompile(path);
-        ContinuousIntegrationServer.mavenTest(path);
-        assertTrue( true );
+        boolean test_result = ContinuousIntegrationServer.mavenTest(path);
+        assertFalse(test_result);
     }
 }
