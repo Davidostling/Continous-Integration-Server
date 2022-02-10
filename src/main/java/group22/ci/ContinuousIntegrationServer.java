396e97a1918f3fb56f99a5ea3a038902ad2fab82
Johan <jekbe@kth.se>
2022-02-10 20:01:05 +0100

    Fix issue/15
    Fixes bugs in maven function.
    Imports needed components.

--- src/main/java/group22/ci/ContinuousIntegrationServer.java
+++ src/main/java/group22/ci/ContinuousIntegrationServer.java
@@ -4,14 +4,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import org.json.*;
 
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -57,11 +60,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         } else if(request.getMethod() == "GET"){
             System.out.println("GET");
         }
-
-       
         response.getWriter().println("CI job done");
-
-
     }
 
     public void setPayLoadHandler() throws Exception{
@@ -76,18 +75,15 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 	*/
 	public static MavenResult mavenCompile(String mavenPath) throws IOException{
 		MavenResult temp;
-		
 		try{
 			// The path to the location of the maven project
 			String path = mavenPath;
-			
-			
 			// Run the maven compile command and store output
-            ArrayList output = runCommand("cmd.exe /c mvn -f " + path + "pom.xml compile");
-			
+            ArrayList<String> output = runCommand("cmd.exe /c mvn -f " + path + "pom.xml compile");
+
 			//Store exit status
-			int exitStatus = Integer.parseInt(output.get(output.size() - 1).toString());
-			
+			int exitStatus = Integer.parseInt(output.get(output.size() - 1));
+
 			// Create a MavenResult instance depending on the results of running maven compile
 			if(exitStatus == 0){
 				temp = new MavenResult(true, "Project compiled successfully");
@@ -97,21 +93,16 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 				temp = new MavenResult(false, "Project failed to compile due to an compilation error");
 				return temp;
 			}
-			
 			for (int i = 0; i < output.size(); i++){
-                        System.out.println(output.get(i));
+                System.out.println(output.get(i));
 			}
-				
-			
-        }   
-            catch (IOException e) { 
-                System.err.println(e); 
-            }
-		
+        }
+        catch (IOException e) { 
+            System.err.println(e); 
+        }
 		// Something has gone wrong
 		temp = new MavenResult(false, "ERROR");
 		return temp;
-			
 	}
 	
 	/*
@@ -126,14 +117,10 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 		try{
 			// The path to the location of the maven project
 			String path = mavenPath;
-			
-			
             // Run the maven test command and store output
-            ArrayList output = runCommand("cmd.exe /c mvn -f " + path + "pom.xml test");
-			
+            ArrayList<String> output = runCommand("cmd.exe /c mvn -f " + path + "pom.xml test");
 			//Store exit status
 			int exitStatus = Integer.parseInt(output.get(output.size() - 1).toString());
-			
 			// Create a MavenResult instance depending on the results of running maven test
 			if(exitStatus == 0){
 				temp = new MavenResult(true, "All tests have passed been cleared successfully");
@@ -143,51 +130,38 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 				temp = new MavenResult(false, "At least one test has failed");
 				return temp;
 			}
-			
-			
 			for (int i = 0; i < output.size(); i++){
                         System.out.println(output.get(i));
 			}
-				
-			
-        }   
+        }
             catch (IOException e) { 
                 System.err.println(e); 
             }
-		
 		// Something has gone wrong
 		temp = new MavenResult(false, "ERROR");
 		return temp;
 	}
-	
-	
-	
+
 	/*
 	* Function that runs a command determined by the given parameter
 	*
 	* @param cmd		a String representation of the command to run
 	* @return ArrayList	an ArrayList representing the resulting output of running the command
 	*/
-	static public String[] runCommand(String cmd) throws IOException{
-		
+	static public ArrayList<String> runCommand(String cmd) throws IOException{
         // Create a list for storing output.
-        ArrayList output = new ArrayList();
-		
+        ArrayList<String> output = new ArrayList<>();
         // Execute a command and get its process handle
         Process proc = Runtime.getRuntime().exec(cmd);
-		
         // Get the handle for the processes InputStream
         InputStream input = proc.getInputStream();
-		
         // Create a BufferedReader and set it to read from from inputstream
         BufferedReader br = new BufferedReader(new InputStreamReader(input));
         String temp;
-		
 		// As long as the current next line in BufferedReader is not null
 		// Add to output list
         while ((temp = br.readLine()) != null) 
             output.add(temp);
-		
             // Wait for process to terminate and catch any Exceptions.
             try { 
                 proc.waitFor(); 
@@ -195,12 +169,9 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             catch (InterruptedException e) {
                 System.err.println("Process was interrupted"); 
                 }
-				
 			// Add the exit value to the ArrayList
-			output.add(proc.exitValue());
-				
+			output.add(String.valueOf(proc.exitValue()));
             br.close();
-		
             return output;
 	}
 	
